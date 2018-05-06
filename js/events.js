4c096e424c487d9b95ce9412bfbc3d394603fcde
Liz Gunter <midwestunicorn@gmail.com>
2018-05-06 23:31:05 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+    return;
+  })
+}
 
+function frameIt() {
+  $('img').('load', function () {
+    img.attr('.tasty')
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function () {
+    if(key.which == 71){
+      alert('g was pressed');
+      return;
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function () {
+    alert "Your form is going to be submitted now.";
+    return;
+  })
+}
+
+$(document).ready(function (){
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
+
 
 });
