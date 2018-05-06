74b493af9c6e5c1ed17c0782fca17b7d1cdb704c
Liz Gunter <midwestunicorn@gmail.com>
2018-05-06 23:46:48 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+    return;
+  });
+}
 
+function frameIt() {
+  $('img').on('load', function () {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('form').on('keydown', function (key) {
+    if(key.which == 71){
+      alert('g was pressed');
+      return;
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function () {
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
+
+$(document).ready(function (){
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
+
 
 });
