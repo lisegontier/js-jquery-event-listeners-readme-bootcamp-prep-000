b5c77d025e0c6f89269f38ba32cb649869ed6473
Liz Gunter <midwestunicorn@gmail.com>
2018-05-06 23:43:53 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form').on('keydown', function () {
+  $('form').on('keydown', function (key) {
     if(key.which == 71){
       alert('g was pressed');
       return;
